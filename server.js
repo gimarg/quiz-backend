ae51b8af21d68e0be7ee50cebb375ddb1e10fae3
Georgia <gimarg21@gmail.com>
2025-05-06 17:13:14 +0300

    Add extra questions file to serve

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/questions", (req, res) => {
-  fs.readFile("./questions.json", "utf8", (err, data) => {
+app.get("/playerQuestions", (req, res) => {
+  fs.readFile("./playerQuestions.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to load questions" });
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get("/assistantQuestions", (req, res) => {
+  fs.readFile("./assistantQuestions.json", "utf8", (err, data) => {
     if (err) {
       res.status(500).json({ error: "Failed to load questions" });
       return;
